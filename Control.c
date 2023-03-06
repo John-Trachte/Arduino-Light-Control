d8b192cc4903455e50f7eba1cda855309a0f7f97
John Trachte <tracht03j@gmail.com>
2023-03-06 11:21:20 -0600

    Load shaders from .glsl files

--- Control.c
+++ Control.c
@@ -4,6 +4,28 @@
 #include <stdio.h>
 #include <string.h>
 
+char* readShader(const char* file)
+{
+    FILE* fp; 
+    long size = 0;
+    char* shader;
+
+    fp = fopen(file, "rb");
+    if(fp == NULL)
+      return "";
+
+    fseek(fp, 0L, SEEK_END);
+    size = ftell(fp) + 1;
+    fclose(fp);
+
+    fp = fopen(file, "r");
+    shader = memset(malloc(size), '\0', size);
+    fread(shader, 1, size - 1, fp);
+    fclose(fp);
+
+    return shader;
+}
+
 int main()
 {
     if(!glfwInit())
@@ -56,24 +78,14 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL); // variables made from 3 floats
 
-    // temporary shaders
-    const char* vertex_shader =
-    "#version 400\n" 
-    "in vec3 vp;"
-    "void main() {"
-    " gl_Position = vec4(vp, 1.0);"
-    "}";
 
-    const char* fragment_shader =
-    "#version 400\n"
-    "out vec4 frag_colour;"
-    "void main() {"
-    " frag_colour = vec4(0.5, 0.0, 0.5, 1.0);" 
-    "}";
+    const char* vertex_shader = readShader("Vertex.glsl");
 
     GLuint vs = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vs, 1, &vertex_shader, NULL);
     glCompileShader(vs);
+
+    const char* fragment_shader = readShader("Fragment.glsl");
     
     GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(fs, 1, &fragment_shader, NULL);
