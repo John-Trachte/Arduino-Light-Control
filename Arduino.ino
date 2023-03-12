eedefb58bc2a4a83ca5798987bc80c2fca92eae8
John Trachte <tracht03j@gmail.com>
2023-03-11 23:51:40 -0600

    Add basic serial read

--- Arduino.ino
+++ Arduino.ino
@@ -1,17 +1,25 @@
 void setup()
 {
-	pinMode(3, OUTPUT); // green led
-	pinMode(5, OUTPUT); // yellow 
-	pinMode(6, OUTPUT); // blue 
-	pinMode(9, OUTPUT); // red 
+  Serial.begin(9600);
+  
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
 }
+
+int greenScale = 1, yellowScale = 1, blueScale = 1, redScale = 1;
+
 
 void loop()
 {
-    float greenScale = 1, yellowScale = 1, blueScale = 1, redScale = 1;
+    greenScale = Serial.read();
+    yellowScale = Serial.read();
+  	blueScale = Serial.read();
+    redScale = Serial.read();
 
-    analogWrite(3, greenScale * 255);
-    analogWrite(5, yellowScale * 255);
-    analogWrite(6, blueScale * 255);
-    analogWrite(9, redScale * 255);
+    analogWrite(3, (greenScale * 255) / 1000);
+    analogWrite(5, (yellowScale * 255) / 1000);
+    analogWrite(6, (blueScale * 255) / 1000);
+    analogWrite(9, (redScale * 255)  / 1000);
 }
